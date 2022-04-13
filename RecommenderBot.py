96c8f0e70894152035f22504a58f1fbef23ab350
dheerajsavanthy <91928767+dheerajsavanthy@users.noreply.github.com>
2022-04-13 19:37:40 -0400

    Add files via upload

---
+++ RecommenderBot.py
@@ -0,0 +1,25 @@
+import discord
+from neuralintents import GenericAssistant
+import os 
+import nltk
+
+chatbot = GenericAssistant(r'C:\Users\Dheeraj\Downloads\intents.json')
+chatbot.train_model()
+chatbot.save_model()
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!bot'):
+        response = chatbot.request(message.content[5:])
+        await message.channel.send(response)
+    
+
+    
+
+client.run('OTUyODMzMjcyNzgwNTg3MDM5.Yi7xIw.E06xUUwTEF1roz_SZsAR6e_6_1A')
